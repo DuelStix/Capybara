a6ccd686649f40d18a01d707eb625c0476a825d4
oven-schmuvven2 <166764050+oven-schmuvven2@users.noreply.github.com>
2025-04-18 10:09:18 -0700

    Add files via upload

---
+++ gameforclass/Capybara-main/gameforclass/scenes/title_screen.gd
@@ -0,0 +1,13 @@
+extends CanvasLayer
+
+
+func _on_options_button_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_exit_button_pressed() -> void:
+	get_tree().quit()
+
+
+func _on_start_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/main.tscn")
